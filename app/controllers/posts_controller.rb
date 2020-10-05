6b638231cc06ac33e7fc7afb69a973911b74b1c9
Shota-Todoroki <71693760+Shota-Todoroki@users.noreply.github.com>
2020-10-05 12:34:06 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,22 @@ class PostsController < ApplicationController
   def index  # indexアクションを定義した
     @posts = Post.all.order(id: "DESC")  #一番目のレコードを＠postに代入
   end
-  
-  
-  
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
-      
+    redirect_to action: :index  
+  end
+
+  def checked
+    post = Post.find(params[:id])#ルーティングで既読したメモのidをコントローラーに渡している。
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
  
-
 end
